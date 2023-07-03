470128fa9848fb1d21b422b87f7b3a8d7dacbde3
Alfred Marku <84595876+AlfredMarku@users.noreply.github.com>
2023-07-03 12:17:55 +0200

    Update app.js

--- app.js
+++ app.js
@@ -28,3 +28,12 @@ function age() {
 
 
 }
+
+function isNumber(evt) {
+    evt = (evt) ? evt : window.event;
+    var charCode = (evt.which) ? evt.which : evt.keyCode;
+    if (charCode > 31 && (charCode < 48 || charCode > 57)) {
+        return false;
+    }
+    return true;
+}
